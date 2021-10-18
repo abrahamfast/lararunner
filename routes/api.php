74003699637868745b6d11b4012c61df61bca32e
Abraham ghaemi <ig.ghaemi@gmail.com>
2021-10-18 10:00:10 +0330

    add new routes

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,43 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\User;
-use App\Http\Controllers\Api\Auth\Token;
-use App\Http\Controllers\Api\Auth\Register;
-
-Route::post('/auth/token', Token::class);
-Route::post('/auth/register', Register::class);
-
-Route::middleware('auth:sanctum')->get('/user', User::class);
 
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
+Route::post('/user/register', function(Request $request){
+    $data = $request->all();
 
+    $user = App\Models\User::create($data);
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::post('/user/login', function(Request $request){
+    $dataJson = $request->json();
+
+    $user = App\Models\User::where('email', $dataJson->get('email'))->first();
+
+    dd($request->password, $user->password);
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+    dd($dataJson);
+});
+
+Route::get('/resturant', function(Request $request){
+    $resturants = App\Models\Resturant::all();
+
+    return $resturants;
+});
+
+Route::get('/resturant/{id}', function(Request $request, $id){
+    return App\Models\Resturant::findOrfail($id);
+});
